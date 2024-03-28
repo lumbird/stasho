b83ca1f535c88eb370be260cf8e8936b798dece0
Lumie (LumBird) <77795220+lumbird@users.noreply.github.com>
2024-03-28 13:00:47 +0000

    chore: flexibility to action comparisons and action context

--- src/interfaces/store-interfaces.ts
+++ src/interfaces/store-interfaces.ts
@@ -12,6 +12,15 @@ export interface Action {
     type: string;
 }
 
+export interface ActionCallbackContext {
+    type: string;
+    equals: (equalToo: Action | string) => boolean;
+}
+
+export type ActionCallbackWithParams<T> = ((params: T) => Action) & ActionCallbackContext;
+export type ActionCallbackWithoutParams = (() => Action) & ActionCallbackContext;
+
+
 export type EffectCallback<A extends Action> = (action: A) => void;
 export type ReducerCallback<S extends State, A extends Action> = (oldState: S, action: A) => S;
 export type MemoryCallback<IV, OV> = (state: IV) => OV;
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,11 @@
-import { State, Action, EffectCallback, MemoryCallback, ReducerCallback } from "./interfaces/store-interfaces";
+import {
+    State,
+    Action,
+    EffectCallback,
+    MemoryCallback,
+    ReducerCallback,
+    ActionCallbackWithParams, ActionCallbackWithoutParams, ActionCallbackContext
+} from "./interfaces/store-interfaces";
 
 export function buildMemory<T extends MemoryCallback<any, any>[]>(...callbacks: [...T]) {
     return function (input: Parameters<T[0]>[0]) {
@@ -16,25 +23,67 @@ export function buildEffect<T extends EffectCallback<any>[]>(...callbacks: [...T
     return (action: Parameters<T[0]>[0]) => callbacks.forEach((effectCallback) => effectCallback(action));;
 }
 
-export function isAction(actionA: Action, actionB: Action): boolean {
-    return actionA.type === actionB.type;
+export function isAction(
+    actionA: string | Action | ActionCallbackContext,
+    actionB: string | Action | ActionCallbackContext
+): boolean {
+
+    if (typeof(actionA) === "object" && typeof(actionB) === "object")
+    {
+        return actionA.type === actionB.type;
+    }
+    else if(typeof(actionA) === "object") {
+        return actionA.type === actionB;
+    }
+    else if(typeof(actionB) === "object") {
+        return actionA === actionB.type;
+    }
+    else {
+        return actionA === actionB;
+    }
 }
 
-export function actionWithParams<T extends object>(type: string): (params: T) => Action & T {
-    return (params: T) => {
+export function actionWithParams<T extends object>(type: string): ActionCallbackWithParams<T> {
+    const callback: ((params: T) => Action) = (params: T) => {
         return {
             type,
             ...params
         }
-    }
+    };
 
+    const callbackContext: ActionCallbackContext = {
+        type,
+        equals: (action: string | Action | ActionCallbackContext): boolean => {
+            if (typeof action == "object") {
+                return action.type === type;
+            }
+            else {
+                return action === type;
+            }
+        }
+    };
 
+    return Object.assign(callback, callbackContext);
 }
 
-export function actionWithoutParams(type: string) {
-    return () => ({
+export function actionWithoutParams(type: string): ActionCallbackWithoutParams {
+    const callback = () => ({
         type
-    })
+    });
+
+    const callbackContext: ActionCallbackContext = {
+        type,
+        equals: (action: string | Action | ActionCallbackContext): boolean => {
+            if (typeof action == "object") {
+                return action.type === type;
+            }
+            else {
+                return action === type;
+            }
+        }
+    };
+
+    return Object.assign(callback, callbackContext);
 }
 
 
